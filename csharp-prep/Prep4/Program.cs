1cbdd58be1c89433487f190c4a68d792448a3555
brightonkbgrow <brightongrow@byui.edu>
2024-04-26 14:58:49 -0600

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userdigit = -1;
+        
+        while (userdigit != 0)
+        {
+            Console.WriteLine("Input numbers, enter 0 to stop.");
+            string userinput = Console.ReadLine();
+            userdigit = int.Parse(userinput);
+
+            if (userdigit != 0)
+            {
+                numbers.Add(userdigit);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"Sum: {sum}");
+
+        int average = sum / numbers.Count;
+        Console.WriteLine($"Average: {average}");
+
+        int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+            Console.WriteLine($"Max: {max}");
     }
 }
