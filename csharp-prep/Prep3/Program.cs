b0d83d96732d724e0d579322abf9935595542cea
brightonkbgrow <brightongrow@byui.edu>
2024-04-26 14:27:32 -0600

    Prep3 Done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        string again = "YES";
+        while (again == "YES")
+        {
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int guess = 1;
+        int tries = 0;
+        
+        Console.WriteLine("Guess a number! ");
+
+            while (!(guess == number))
+            {   
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                    tries = tries + 1;
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                    tries = tries + 1;
+                }
+                else
+                {
+                    Console.WriteLine("You've Guess It!");
+                    Console.WriteLine($"You took {tries} tries");
+                    Console.WriteLine($"Play Again? YES/NO");
+                    again = Console.ReadLine();
+                }
+            }
+
+
+
+
+        }
     }
 }
