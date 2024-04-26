80b0dc640965f2df2506376184d07f5698ee6a4f
brightonkbgrow <brightongrow@byui.edu>
2024-04-26 13:52:23 -0600

    complete program 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What percentage did you get? ");
+        string percentage = Console.ReadLine();
+        int percent = int.Parse(percentage);
+        if (percent >= 90)
+        {
+            Console.WriteLine("Your Grade is an A");
+        }
+        else if (percent >= 80)
+        {
+            Console.WriteLine("Your Grade is an B");
+        }
+        else if (percent >= 70)
+        {
+            Console.WriteLine("Your Grade is an C");
+        }
+        else if (percent >= 60)
+        {
+            Console.WriteLine("Your Grade is an D");
+        }
+        else if (percent < 60)
+        {
+            Console.WriteLine("You got an F");
+        }
+        else
+        {
+            Console.WriteLine("That is not a possible answer....");
+        }
     }
+
 }
